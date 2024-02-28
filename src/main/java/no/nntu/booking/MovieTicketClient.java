00e7c3a59f2c03f1d59e3041076e3dc7e4c366b3
Sander Grimstad <sander.grimstad00@gmail.com>
2024-02-28 09:44:13 +0100

    changed the file structure

--- src/main/java/no/nntu/booking/MovieTicketClient.java
+++ src/main/java/no/nntu/booking/MovieTicketClient.java
@@ -3,8 +3,10 @@ package no.nntu.booking;
 public class MovieTicketClient extends Thread {
   private String customerName;
   private int numberOfTickets;
+  private MovieTicketServer server;
 
   public MovieTicketClient(MovieTicketServer server, String customerName, int numberOfTickets) {
+    this.server = server;
     this.customerName = customerName;
     this.numberOfTickets = numberOfTickets;
     //TODO: TBD
